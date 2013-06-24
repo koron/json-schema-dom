447e303d3c262f12512d819aa6eafd8b642ba3b3
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-06-24 16:38:52 +0900

    add pattern for string

--- src/main/java/net/kaoriya/qb/json/schema/dom/StringValidation.java
+++ src/main/java/net/kaoriya/qb/json/schema/dom/StringValidation.java
@@ -1,5 +1,7 @@
 package net.kaoriya.qb.json.schema.dom;
 
+import java.util.regex.Pattern;
+
 public interface StringValidation extends InstanceValidation
 {
     // 5.2.1
@@ -7,4 +9,7 @@ public interface StringValidation extends InstanceValidation
 
     // 5.2.2
     Integer getMinLength();
+
+    // 5.2.3
+    Pattern getPattern();
 }
